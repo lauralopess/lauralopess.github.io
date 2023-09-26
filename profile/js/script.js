6061dafc2946cd80b14fd614dbdbe05dd9f9dec9
lauralopess <141637154+lauralopess@users.noreply.github.com>
2023-09-26 11:51:24 -0300

    Delete profile directory

--- profile/js/script.js
+++
@@ -1,7 +0,0 @@
-function enviarMensagem() {
-    var nome = document.getElementById("idNome");
-
-    if(nome.value !="") {
-        alert("Obrigado, sr(a) " + nome.value + ", sua mensagem foi enviada com sucesso!")
-    }
-}
